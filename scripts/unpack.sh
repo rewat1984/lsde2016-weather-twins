487813cc3f548a25f4185203f639187d18740630
Bram Rodenburg <b.p.m.rodenburg@vu.nl>
2016-03-06 08:42:30 +0100

    Created a unzip script

---
+++ scripts/unpack.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/bash
+
+for ((t=1903; t<=2016; t++))
+do
+        echo "Processing year $t ...."
+        hadoop fs -copyToLocal /user/hannesm/lsde/noaa/"$t".zip "$t".zip
+        unzip "$t".zip
+        hadoop fs -mkdir /user/lsde02/data/"$t"
+        hadoop fs -put "$t"/*.gz /user/lsde02/data/"$t"
+        rm "$t".zip
+        rm -rf "$t"/
+        echo "Finished processing year $t."
+done
