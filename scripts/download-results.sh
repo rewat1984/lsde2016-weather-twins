da59d9e204be0d9cfdb91d3ae5e8d43ca8812a51
Bram Rodenburg <b.p.m.rodenburg@vu.nl>
2016-03-22 13:27:30 +0100

    Fixed bug

--- scripts/download-results.sh
+++ scripts/download-results.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 mkdir $1
-hadoop fs -copyToLocal /user/lsde02/$1-sky-condition $1/sky-condition
-hadoop fs -copyToLocal /user/lsde02/$1-temp $1/temp
-hadoop fs -copyToLocal /user/lsde02/$1-wind-speed $1/wind-speed
-hadoop fs -copyToLocal /user/lsde02/$1-visibility $1/visibility
+hadoop fs -copyToLocal /user/lsde02/results/$1-sky-condition $1/sky-condition
+hadoop fs -copyToLocal /user/lsde02/results/$1-temp $1/temp
+hadoop fs -copyToLocal /user/lsde02/results/$1-wind-speed $1/wind-speed
+hadoop fs -copyToLocal /user/lsde02/results/$1-visibility $1/visibility
